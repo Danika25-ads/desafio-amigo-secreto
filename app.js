eb081089f1e698340280d8a0dcb7ebfbf3a5b755
Danika25-ads <daniela.sorriso@gmail.com>
2025-03-17 13:30:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação 
+let amigos = [];
+
+    function adicionarAmigo() {
+        let inputAmigo = document.getElementById("amigo");
+        let nomeAmigo = inputAmigo.value; 
+
+    if (!nomeAmigo) {
+        alert("Favor insira um nome válido!"); 
+        return;
+    }
+    amigos.push(nomeAmigo);
+    inputAmigo.value = ""; // Limpa o campo após adicionar
+    inputAmigo.focus(); 
+    atualizarLista();
+    }
+
+    function atualizarLista () {
+        let listaAmigos = document.getElementById("listaAmigos");
+        listaAmigos.innerHTML = ""; 
+
+        for(let i = 0; i < amigos.length; i++) {
+            let item = document.createElement("li");
+            item.textContent = amigos [i];
+            listaAmigos.appendChild (item);
+        }
+    }
+
+    function sortearAmigo() {
+        if (amigos.length === 0){
+            alert("Nenhum amigo adicionado");
+            return;
+        }
+        let sorteado = amigos[Math.floor(Math.random()*amigos.length)];
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `O amigo sorteado é ${sorteado}`; 
+
+    let limparLista = document.getElementById ('listaAmigos')
+    limparLista.innerHTML = "";
+    }
